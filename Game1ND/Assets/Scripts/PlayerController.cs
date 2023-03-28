9e72fffe95e3cd74cd9099d46f32fe59efb56053
NickDeAssis <nick.deassis94@gmail.com>
2023-03-28 00:50:28 -0700

    Stuff

---
+++ Game1ND/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10.0f;
+    private Rigidbody playerRb;
+    private float zBoundTop = 9.0f;
+    private float zBoundBot = -9.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        playerRb.AddForce(Vector3.forward * speed * verticalInput);
+        playerRb.AddForce(Vector3.right * speed * horizontalInput);
+
+        if (transform.position.z > zBoundTop)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zBoundTop);
+        }
+
+        if (transform.position.z < zBoundBot)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zBoundBot);
+        }
+    }
+}
