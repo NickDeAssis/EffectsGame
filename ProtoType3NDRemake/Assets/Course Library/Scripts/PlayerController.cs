fac2874a7422dc3b0c90ef4c5bcba792fac12931
NickDeAssis <nick.deassis94@gmail.com>
2023-04-12 21:12:53 -0700

    Remake of game

--- ProtoType3NDRemake/Assets/Course Library/Scripts/PlayerController.cs
+++ ProtoType3NDRemake/Assets/Course Library/Scripts/PlayerController.cs
@@ -8,13 +8,19 @@ public class PlayerController : MonoBehaviour
     public bool isOnGround = true;
     public float jumpForce = 10.0f;
     public float gravityModifier;
+    public ParticleSystem explostionParticle;
+    public ParticleSystem dirtParticle;
     private Rigidbody playerRb;
     private Animator playerAnim;
+    private AudioSource playerAudioSource;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
         playerAnim = GetComponent<Animator>();
+        playerAudioSource = GetComponent<AudioSource>();
         Physics.gravity *= gravityModifier;
         //playerRb.AddForce(Vector3.up * 1000);
     }
@@ -25,8 +31,11 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            dirtParticle.Stop();
+            playerAudioSource.PlayOneShot(jumpSound, 1.0f);
             isOnGround = false;
             playerAnim.SetTrigger("Jump_trig");
+            
         }
     }
 
@@ -34,12 +43,16 @@ public class PlayerController : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
+            dirtParticle.Play();
             isOnGround = true;
         }else if (collision.gameObject.CompareTag("Obstacle"))
         {
             gameOver = true;
             playerAnim.SetBool("Death_b", true);
             playerAnim.SetInteger("DeathType_int", 1);
+            explostionParticle.Play();
+            dirtParticle.Stop();
+            playerAudioSource.PlayOneShot(crashSound, 1.0f);
             Debug.Log("Game Over!");
         }
 
